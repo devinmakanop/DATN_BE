f444cf0af651e115a08dfa3f0c06960ffc3c4f26
nguyendan <dan3366881@gmail.com>
2025-06-01 20:39:01 +0700

    ok

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const Admin = require('../models/admin')
 
 const JWT_SECRET = process.env.JWT_SECRET || 'mysecretkey';
 
@@ -53,6 +54,23 @@ router.post('/login', async (req, res) => {
         const token = jwt.sign({ id: user._id, username: user.username }, JWT_SECRET, { expiresIn: '7d' });
 
         res.json({ token, user});
+    } catch (err) {
+        res.status(500).json({ message: 'Login failed', error: err.message });
+    }
+});
+
+router.post('/admin/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const admin = await Admin.findOne({ username });
+        if (!admin) return res.status(401).json({ message: 'Invalid credentials' });
+
+        const isMatch = await admin.comparePassword(password);
+        if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
+
+        const token = jwt.sign({ id: admin._id, username: admin.username }, JWT_SECRET, { expiresIn: '7d' });
+
+        res.json({ token, admin});
     } catch (err) {
         res.status(500).json({ message: 'Login failed', error: err.message });
     }
